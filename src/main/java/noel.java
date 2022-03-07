a4f11d09f3af8ceaff372c960050109ecc15f27c
Noel <tegarnoel83@gmail.com>
2022-03-07 14:34:47 +0700

    adding new file java

---
+++ src/main/java/noel.java
@@ -0,0 +1,6 @@
+public class noel {
+    public static void main(String[] args) {
+        System.out.println("yes, no");
+    }
+    
+}
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello my name is Noel");
+        System.out.println("hello my name is Tegar Noel");
     }
 }
